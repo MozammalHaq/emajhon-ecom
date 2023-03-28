3257dd84487df5fc081bb0356c9c89a0a58c75a6
MozammalHaq <mrmozammal@gmail.com>
2023-03-28 18:55:06 +0600

    shop and product page updated

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const { img, name, price, seller, ratings } = props.product
+    const handleAddToCart = props.handleAddToCart;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className="product-info">
+                <h6>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Menufacturer: {seller}</p>
+                <p>{ratings}</p>
+            </div>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
